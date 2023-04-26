4e350f2c8dbf0a5a41071aed0730ea5c5d241c35
PolyBytes <drtylero80@gmail.com>
2023-04-26 01:22:05 -0400

    Finished Player Animation Tree
    All player animation states behave as expected now (I think? lmao). I had to take the equivalent of a crash course in the AnimationTree and AnimationPlayer nodes. I wish I could delete the Start and End nodes in the AnimationTree graph because they are not useful in our case and they are just kind of there. =(
    
    Basic Overview:
    - Player can only open/close the Parasol from the Idle animation
    - Player can only Jump from the Idle or Running animations
    - Player can only Attack when the Parasol is closed
    - Player Falls slower while the Parasol is open
    - Player can Attack in alternate directions by holding the attack_modifier_up or attack_modifier_down keys (W and S by default)

--- Characters/Player/Player.gd
+++ Characters/Player/Player.gd
@@ -4,29 +4,35 @@ extends CharacterBody2D
 @export var ACCELERATION: float = 30.0
 @export var JUMP_VELOCITY: float = -600.0
 @export var MAX_WALK_VELOCITY: float = 300.0
+@export var PARASOL_GRAVITY_MODIFIER: float = 0.3
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var animation_state = animation_tree.get("parameters/playback")
 @onready var sprite: Sprite2D = $Sprite2D
-@onready var parasol_cooldown: Timer = $ParasolCooldown
 @onready var attack_cooldown: Timer = $AttackCooldown
 
 # The initial x scale of the sprite to be used to flip the sprite and preserve its position
 # so that it will stay within the CollisionShape2D.
 var sprite_scale_x: float
 
-
+# The currently playing animation from the AnimationTree node
 var current_animation_state: StringName
 
 # This enum represents the different possible states of the Parasol animation
 enum PARASOL_STATES {
 	CLOSED = -1,
-	OPENING = 0,
 	OPEN = 1
 }
 
-var parasol_state = PARASOL_STATES.CLOSED
-#var parasol_gravity_modifier: float = 1 #currently not working??
+# This enum represents the different possible states of the Parasol Attack animation direction
+enum ATTACK_DIRECTION_STATES {
+	DOWN = -1,
+	FRONT = 0,
+	UP = 1
+}
+
+var parasol_state: PARASOL_STATES = PARASOL_STATES.CLOSED
+var attack_direction_state: ATTACK_DIRECTION_STATES = ATTACK_DIRECTION_STATES.FRONT
 
 var is_attacking = false
 
@@ -35,17 +41,21 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")# * paras
 var direction = Vector2.ZERO
 
 func _ready():
-	update_animation_tree_blend_positions()
-	animation_state.start("Start")
-	animation_state.travel("Idle")
+	update_parasol_animation_blend_positions()
+	animation_state.start("Idle")
 	sprite_scale_x = sprite.scale.x
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		if parasol_state == PARASOL_STATES.OPEN and current_animation_state == "Fall":
+			velocity.y += (gravity * PARASOL_GRAVITY_MODIFIER) * delta
+		else:
+			velocity.y += gravity * delta
 
 	current_animation_state = animation_state.get_current_node()
+
+	update_current_attack_direction_state()
 
 	if Input.is_action_just_pressed("toggle_parasol") and current_animation_state == "Idle":
 		match parasol_state:
@@ -56,11 +66,14 @@ func _physics_process(delta):
 				animation_state.travel("close_parasol")
 				parasol_state = PARASOL_STATES.CLOSED
 		
-		update_animation_tree_blend_positions()
-	elif Input.is_action_just_pressed("jump") and is_on_floor():
+		update_parasol_animation_blend_positions()
+	elif Input.is_action_just_pressed("jump") and is_on_floor() and (current_animation_state == "Idle" or current_animation_state == "Run"):
+		animation_state.travel("Jump")
 		velocity.y = JUMP_VELOCITY
 	elif Input.is_action_pressed("attack") and attack_cooldown.is_stopped() and parasol_state == PARASOL_STATES.CLOSED:
 		attack_cooldown.start()
+		animation_tree.set("parameters/Attack/blend_position", attack_direction_state)
+		animation_state.travel("Attack")
 		MAX_WALK_VELOCITY *= 0.5
 
 	# Get the input direction and handle the movement/deceleration.
@@ -81,6 +94,10 @@ func update_animation():
 		animation_state.travel("Idle")
 	elif direction.x != 0.0 and current_animation_state == "Idle":
 		animation_state.travel("Run")
+	elif current_animation_state == "Jump" and velocity.y > 0:
+		animation_state.travel("Fall")
+	elif current_animation_state == "Fall" and is_equal_approx(velocity.y, 0.0):
+		animation_state.travel("Idle")
 
 func update_direction():
 	if direction.x < 0:
@@ -92,13 +109,16 @@ func _on_attack_cooldown_timeout():
 	is_attacking = false
 	MAX_WALK_VELOCITY *= 2
 
-func _on_parasol_cooldown_timeout():
-	match parasol_state:
-		PARASOL_STATES.OPENING:
-			parasol_state = PARASOL_STATES.OPEN
-		PARASOL_STATES.OPEN:
-			parasol_state = PARASOL_STATES.CLOSED
-
-func update_animation_tree_blend_positions():
+func update_parasol_animation_blend_positions():
 	animation_tree.set("parameters/Idle/blend_position", parasol_state)
 	animation_tree.set("parameters/Run/blend_position", parasol_state)
+	animation_tree.set("parameters/Jump/blend_position", parasol_state)
+	animation_tree.set("parameters/Fall/blend_position", parasol_state)
+
+func update_current_attack_direction_state():
+	if Input.is_action_pressed("attack_modifier_down"):
+		attack_direction_state = ATTACK_DIRECTION_STATES.DOWN
+	elif Input.is_action_pressed("attack_modifier_up"):
+		attack_direction_state = ATTACK_DIRECTION_STATES.UP
+	else:
+		attack_direction_state = ATTACK_DIRECTION_STATES.FRONT

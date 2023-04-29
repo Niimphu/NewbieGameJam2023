f80c6f7b39a86715f26a089d48880594764943c9
Niimphu <nimphu@outlook.com>
2023-04-29 22:09:18 +0200

    little bit of debugging and refactoring

--- Characters/Player/Player.gd
+++ Characters/Player/Player.gd
@@ -4,20 +4,17 @@ extends CharacterBody2D
 @export var ACCELERATION: float = 40.0
 @export var JUMP_VELOCITY: float = -460.0
 @export var MAX_WALK_VELOCITY: float = 250.0
-@export var PARASOL_GRAVITY_MODIFIER: float = 0.3
+@export var ATTACKING_VELOCITY_MOD: float = 0.5
+@export var PARASOL_GRAVITY_MODIFIER: float = 0.25
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var animation_state = animation_tree.get("parameters/playback")
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var attack_cooldown: Timer = $AttackCooldown
-#@onready var crate = get_node("res://Levels/Environment/Crate/Crate.tscn") #brokenn
 @onready var sprite_scale = sprite.get_scale()
 # The initial x scale of the sprite to be used to flip the sprite and preserve its position
 # so that it will stay within the CollisionShape2D.
-var sprite_scale_x: float
-
-# The currently playing animation from the AnimationTree node
-var current_animation_state: StringName
+#var sprite_scale_x: float = sprite.scale.x
 
 # This enum represents the different possible states of the Parasol animation
 enum PARASOL_STATES {
@@ -32,9 +29,11 @@ enum ATTACK_DIRECTION_STATES {
 	UP = 1
 }
 
+
+# The currently playing animation from the AnimationTree node
+var current_animation_state: StringName
 var parasol_state: PARASOL_STATES = PARASOL_STATES.CLOSED
 var attack_direction_state: ATTACK_DIRECTION_STATES = ATTACK_DIRECTION_STATES.FRONT
-
 var is_attacking = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -69,14 +68,13 @@ func _physics_process(delta):
 
 	if direction.x:
 		velocity.x = clampf(velocity.x + (direction.x * ACCELERATION), -MAX_WALK_VELOCITY, MAX_WALK_VELOCITY)
-		#velocity.x = velocity.x + (direction.x * ACCELERATION)
-		print("velocity.x = ", velocity.x)
+		velocity.x = velocity.x + (direction.x * ACCELERATION)
 	elif is_on_floor():
 		velocity.x = move_toward(velocity.x, 0, DECELERATION)
 
 	if is_on_wall():
 		check_pushable_object()
-
+	print(current_animation_state)
 	update_sprite_direction()
 	update_animation()
 	move_and_slide()
@@ -101,16 +99,19 @@ func player_attack():
 	attack_cooldown.start()
 	animation_tree.set("parameters/Attack/blend_position", attack_direction_state)
 	animation_state.travel("Attack")
-	MAX_WALK_VELOCITY *= 0.5
+	MAX_WALK_VELOCITY *= ATTACKING_VELOCITY_MOD
 
 func update_animation():
-	if is_equal_approx(direction.x, 0.0) and current_animation_state == "Run":
+	if current_animation_state == "open_parasol" or current_animation_state == "close_parasol":
+		return
+	elif is_equal_approx(direction.x, 0.0) and current_animation_state == "Run":
 		animation_state.travel("Idle")
 	elif direction.x != 0.0 and current_animation_state == "Idle":
 		animation_state.travel("Run")
 	elif current_animation_state == "Jump" and velocity.y > 0:
 		animation_state.travel("Fall")
-	elif current_animation_state == "Fall" and is_equal_approx(velocity.y, 0.0):
+#	elif current_animation_state == "Fall" and is_equal_approx(velocity.y, 0.0):
+	elif is_on_floor() and current_animation_state == "Fall":
 		animation_state.travel("Idle")
 
 func update_sprite_direction():
@@ -127,7 +128,7 @@ func check_pushable_object():
 
 func _on_attack_cooldown_timeout():
 	is_attacking = false
-	MAX_WALK_VELOCITY *= 2
+	MAX_WALK_VELOCITY /= ATTACKING_VELOCITY_MOD
 
 func update_parasol_animation_blend_positions():
 	animation_tree.set("parameters/Idle/blend_position", parasol_state)
@@ -136,9 +137,13 @@ func update_parasol_animation_blend_positions():
 	animation_tree.set("parameters/Fall/blend_position", parasol_state)
 
 func update_current_attack_direction_state():
-	if Input.is_action_pressed("attack_modifier_down"):
-		attack_direction_state = ATTACK_DIRECTION_STATES.DOWN
-	elif Input.is_action_pressed("attack_modifier_up"):
-		attack_direction_state = ATTACK_DIRECTION_STATES.UP
+#	if Input.is_action_pressed("attack_modifier_down"):
+#		attack_direction_state = ATTACK_DIRECTION_STATES.DOWN
+#	elif Input.is_action_pressed("attack_modifier_up"):
+#		attack_direction_state = ATTACK_DIRECTION_STATES.UP
+	var vertical_attack_strength = Input.get_action_strength("ui_down") - Input.get_action_strength(("ui_up"))
+	
+	if vertical_attack_strength:
+		attack_direction_state = ATTACK_DIRECTION_STATES.DOWN if vertical_attack_strength > 0 else ATTACK_DIRECTION_STATES.UP
 	else:
 		attack_direction_state = ATTACK_DIRECTION_STATES.FRONT

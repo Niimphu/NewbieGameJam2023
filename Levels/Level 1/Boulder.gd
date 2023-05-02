0df2fee8d4939e25a7b39fef225e586416796464
PolyBytes <drtylero80@gmail.com>
2023-05-02 00:10:04 -0400

    Fixed the Pioneer Rock
    The pioneers used to drive these babies for miles, and it's in great shape!

--- Levels/Level 1/Boulder.gd
+++ Levels/Level 1/Boulder.gd
@@ -1,7 +1,15 @@
 extends RigidBody2D
 
-@export var MAX_VELOCITY: float = 65.0
+@export var MAX_X_VELOCITY: float = 65.0
+@export var MAX_Y_VELOCITY: float = -1.0
 
-func _physics_process(_delta):
-	if linear_velocity.length() > MAX_VELOCITY:
-		linear_velocity = linear_velocity.normalized() * MAX_VELOCITY
+func _integrate_forces(state):
+	var linear_velocity_normalized: Vector2 = state.linear_velocity.normalized()
+
+	if MAX_X_VELOCITY >= 0.0:
+		if abs(state.linear_velocity.x) > MAX_X_VELOCITY:
+			state.linear_velocity.x = linear_velocity_normalized.x * MAX_X_VELOCITY
+	
+	if MAX_Y_VELOCITY >= 0.0:
+		if abs(state.linear_velocity.y) > MAX_Y_VELOCITY:
+			state.linear_velocity.y = linear_velocity_normalized.y * MAX_Y_VELOCITY
